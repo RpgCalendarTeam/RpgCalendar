b66700c5a09ca5ec3ee6012ea9539d61ce1e914a
Mark Studor <mark_studor@hotmail.com>
2020-06-08 18:14:51 -0700

    Removed Username from authentication

--- RPGCalendar/RPGCalendar.Identity/AuthenticationService.cs
+++ RPGCalendar/RPGCalendar.Identity/AuthenticationService.cs
@@ -25,23 +25,13 @@
         }
         public async Task<string?> Login(LoginModel model)
         {
-            if (model.Email is null && model.Username is null)
+            if (model.Email is null)
                 return null;
             string? userId = null;
-            if (model.Username is { })
-            {
-                var result = await _signInManager.PasswordSignInAsync(model.Username,
-                    model.Password, model.RememberMe, lockoutOnFailure: false);
-                if (result.Succeeded)
-                    userId = (await _userManager.Users.FirstAsync(ur => ur.UserName == model.Username)).Id;
-            }
-            else if(model.Email is { })
-            {
-                var user = await _userManager.Users.FirstAsync(ur => ur.Email == model.Email);
-                var result = await _signInManager.PasswordSignInAsync(user.UserName,
-                    model.Password, model.RememberMe, lockoutOnFailure: false);
-                userId = user.Id;
-            }
+            var user = await _userManager.Users.FirstAsync(ur => ur.Email == model.Email);
+            var result = await _signInManager.PasswordSignInAsync(user.UserName,
+                model.Password, model.RememberMe, lockoutOnFailure: false);
+            userId = user.Id;
 
             return userId;
         }
