1b4fcf0800c03f9771a993344cc5b5ccf499ce03
Mark Studor <mark_studor@hotmail.com>
2020-06-11 22:15:04 -0700

    Add Migration

--- RPGCalendar/RPGCalendar.Data/Migrations/SqlServerMigrations/20200612051430_UserAndItemChanges.Designer.cs
+++ RPGCalendar/RPGCalendar.Data/Migrations/SqlServerMigrations/20200612051430_UserAndItemChanges.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RPGCalendar.Data;
 
 namespace RPGCalendar.Data.Migrations.SqlServerMigrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200612051430_UserAndItemChanges")]
+    partial class UserAndItemChanges
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
